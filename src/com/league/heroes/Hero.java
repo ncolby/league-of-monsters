f754d4f8759c1edec2b8d904906eadc07e5cad04
ncolby <85771675+ncolby@users.noreply.github.com>
2023-02-10 00:32:34 -0600

    added a basic hero class to make sure gradle functions

---
+++ src/com/league/heroes/Hero.java
@@ -0,0 +1,17 @@
+package com.league.heroes;
+
+public class Hero {
+    private String name;
+
+    Hero(name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
