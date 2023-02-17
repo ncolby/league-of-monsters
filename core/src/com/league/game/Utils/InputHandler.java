d6559e0f4500c1b342bbb766976e77e5925bd438
ncolby <85771675+ncolby@users.noreply.github.com>
2023-02-17 15:48:56 -0600

    added server link up for connect and disconnect

--- core/src/com/league/game/Utils/InputHandler.java
+++ core/src/com/league/game/Utils/InputHandler.java
@@ -11,41 +11,50 @@ public class InputHandler {
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             if (socket != null) {
                 socket.emit("command", "left");
+                socket.emit("getState");
             }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             if (socket != null) {
                 socket.emit("command", "right");
+                socket.emit("getState");
+
             }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
             if (socket != null) {
                 socket.emit("command", "up");
+                socket.emit("getState");
             }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
             if (socket != null) {
                 socket.emit("command", "down");
+                socket.emit("getState");
             }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.J)) {
             if (socket != null) {
                 socket.emit("command", "skill_1");
+                socket.emit("getState");
             }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.K)) {
             if (socket != null) {
                 socket.emit("command", "skill_2");
+                socket.emit("getState");
             }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.L)) {
             if (socket != null) {
                 socket.emit("command", "skill_3");
+                socket.emit("getState");
             }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.H)) {
             if (socket != null) {
                 socket.emit("command", "skill_4");
+                socket.emit("getState");
             }
         }
     }
