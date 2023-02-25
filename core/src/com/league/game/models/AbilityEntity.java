061e9c3abd3f37e93bd5d0eed24fa1eb540d42eb
hmongkey12 <khammadaolee@gmail.com>
2023-02-25 15:34:39 -0800

    added ability state

--- core/src/com/league/game/models/AbilityEntity.java
+++ core/src/com/league/game/models/AbilityEntity.java
@@ -1,8 +1,14 @@
 package com.league.game.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class AbilityEntity extends GameEntity {
     private long duration;
     private long cooldown;
-    private long translation;
     private long damage;
 }
